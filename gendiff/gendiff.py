bd6747bd698c23237148852215c5b1c9ded82316
Nickolay Berezhencev <nickolay200112@gmail.com>
2024-04-14 15:05:56 +1000

    add

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,11 +5,12 @@ from itertools import chain
 
 def main():
     argparsing()
-    
-    
+
+
 def argparsing():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', type=str) 
+    parser = argparse.ArgumentParser(description='Compares two configuration'
+                                     'files and shows a difference.')
+    parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', type=str, metavar='FORMAT')
     args = parser.parse_args()
@@ -24,18 +25,18 @@ def gener_diff(filename1, filename2):
     for key in keys:
         v1 = js1.get(key)
         v2 = js2.get(key)
-        if v1 != None and v2 != None:
+        if v1 is not None and v2 is not None:
             if js1.get(key) == js2.get(key):
                 res.append(f'  {key} {js1.get(key)}')
             else:
                 res.append(f'- {key} {v1}')
                 res.append(f'+ {key} {v2}')
-        elif v1 != None and v2 == None:
+        elif v1 is not None and v2 is not None:
             res.append(f'- {key} {v1}')
-        elif v1 == None and v2 != None:
+        elif v1 is None and v2 is not None:
             res.append(f'+ {key} {v2}')
     a = '\n'.join(res)
-    return '{\n' +  a + '\n}'
+    return '{\n' + a + '\n}'
 
 
 if __name__ == "__main__":
